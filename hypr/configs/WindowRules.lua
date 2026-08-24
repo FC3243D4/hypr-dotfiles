3f26ae82a10e8c814cc84c83e23528624afba839
FC3243D4 <colacicchi4@gmail.com>
2026-08-24 20:34:29 +0200

    removed reduntant tag

--- hypr/configs/WindowRules.lua
+++ hypr/configs/WindowRules.lua
@@ -100,9 +100,6 @@ hl.window_rule({ match = { class = "^([Ll]ycheeSlicer)$" }, tag = "+threeD" })
 -- git
 hl.window_rule({ match = { class = "^(SourceGit)$" }, tag = "+git" })
 
--- communication
-hl.window_rule({ match = { class = "^([Dd]iscord|[Ww]ebCord|[Vv]esktop)$" }, tag = "+communication" })
-
 -- Gimp
 hl.window_rule({ match = { class = "^(org.gimp.GIMP)$" }, workspace = "4" })
 
@@ -143,7 +140,7 @@ hl.window_rule({ match = { tag = "gamestore*" }, workspace = 9 })
 hl.window_rule({ match = { tag = "git*" }, workspace = 7 })
 hl.window_rule({ match = { tag = "threeD*" }, workspace = 3 })
 hl.window_rule({ match = { tag = "email*" }, workspace = 6 })
-hl.window_rule({ match = { tag = "communication*" }, workspace = 6 })
+hl.window_rule({ match = { tag = "im*" }, workspace = 1 })
 
 -- silent workspace assignments (disabled by default)
 -- hl.window_rule({ match = { tag = "screenshare*" }, workspace = "4 silent" })
